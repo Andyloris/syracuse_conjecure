993d8ffb20160696d93279bfe63bef7281110e64
Andyloris <63070052+Andyloris@users.noreply.github.com>
2021-01-15 18:29:48 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var app = require('express')(),
     PythonShell = require('python-shell')
-pyshell = new PythonShell('syracuse.py')
+pyshell = new PythonShell.PythonShell('syracuse.py')
 app.get('/', function(res, req) {
     pyshell.on('message', (message) => {
         res.send(message)
