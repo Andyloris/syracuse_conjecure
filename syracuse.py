d994bf3cb70ca0bfaf52c210af973ee88041525a
Andyloris <63070052+Andyloris@users.noreply.github.com>
2021-01-15 12:42:05 +0100

    Add files via upload

---
+++ syracuse.py
@@ -0,0 +1,40 @@
+import sys
+
+cache = []
+def filt(k):
+    if(k%2 == 0):
+        return True
+    else:
+        return False
+def invert(b):
+    if(b == True):
+        return False
+    else:
+        return True
+def search():
+    tree = []
+    with open('syracuse_results.txt', 'w+') as dataend:
+        i = 0
+        while i < sys.maxsize:
+            i += 1
+            n = i
+            while n != 4 or n != 2 or n != 1 or n != 0 or invert(n in cache):
+                # print(n)
+                check = filt(n)
+                if(check == True):
+                    n = n / 2
+                    tree.append(n)
+                else:
+                    n = (3*n)+1
+                    tree.append(n)
+                if n == 4.0 or n == 2.0 or n == 1.0 or n == 0.0:
+                    cache.extend(tree)
+                    tree.clear()
+                    # print(str(i) + ' stops\n')
+                    dataend.write(str(i) + ' stops\n')
+                    break
+            else:
+                print('tesr')
+                pass
+
+search()
